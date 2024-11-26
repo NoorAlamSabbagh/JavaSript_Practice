b6eff4e2865f234a21b30327a9228477fbf89029
NoorAlamSabbagh <nooralam7134310@gmail.com>
2024-11-26 10:49:33 +0530

    js revision

--- JavaScript_InterView/JS_Interview_Question_By_Thapa/Day1-25/day1.js
+++ JavaScript_InterView/JS_Interview_Question_By_Thapa/Day1-25/day1.js
@@ -15,24 +15,98 @@
 //? If the input string is empty or contains only whitespace, the function should return as false.
 //? The function should ignore leading and trailing whitespace when determinig the longest word.
 
-const findLongestWord = (str) => {
-// Method 1:
-//    if(str.trim().length === 0){
-//     return false;
-//    }
-//    words = str.split(" ")
-//    words = words.sort((a,b) => b.length - a.length);
-//    return words.at(-1);
-//    return words[0];
-//    console.log(words)
+// const findLongestWord = (str) => {
+// // Method 1:
+// //    if(str.trim().length === 0){
+// //     return false;
+// //    }
+// //    words = str.split(" ")
+// //    words = words.sort((a,b) => b.length - a.length);
+// //    console.log(words)
+// //    //Output
+// // //    [
+// // //     'JavaScriptkjgjkv',
+// // //     'Technical',
+// // //     'Youtube',
+// // //     'course',
+// // //     'watch',
+// // //     'Alam',
+// // //     'on'
+// // //   ]
+// //    return words[0];//JavaScriptkjgjkv
+// //    return words.at(-1);//on
 
-//Method 2:
-    if (str.trim().length === 0) {
-        return false;
+// //Method 2:
+//     if (str.trim().length === 0) {
+//         return false;
+//     }
+//     words = str.split(" ")
+//     return words.reduce((accuWord, currWord) => (currWord.length > accuWord.length ? currWord : accuWord)
+//     );
+// }
+// console.log(findLongestWord("watch Alam Technical JavaScriptkjgjkv course on Youtube"))
+
+
+
+//Practice
+// const findLongestWord = (str) => {
+//     let trimFunction = str.trim().split(" ");
+//     trimFunction = trimFunction.sort((a,b) => b.length - a.length);
+//     console.log("trimFunction", trimFunction)
+//     return trimFunction.at(1);//Technical
+//     return trimFunction.at(-1);//on
+// }
+
+//
+// const findLongestWord = (str) => {
+//     if(str.trim().length === 0){
+//         return false;
+//     }
+//     // words = str.split();
+//     // console.log("words",words) //words [ 'watch Alam Technical JavaScriptkjgjkv course on Youtube' ]
+//     words = str.split(" ");
+//     console.log("words((  ))",words) //Output words((  )) [  'watch',
+// //     'Alam',
+// //     'Technical',
+// //     'JavaScriptkjgjkv',
+// //     'course',
+// //     'on',
+// //     'Youtube'
+// //   ]
+       
+//    return words.reduce((accuWord, currWord) => (currWord.length > accuWord.length ? currWord : accuWord), "")
+// }
+// console.log(findLongestWord("watch Alam Technical JavaScriptkjgjkv course on Youtube"));//Output: JavaScriptkjgjkv
+
+
+
+///////////////////////////////////////
+const findLongestWord = (str) => {
+    let longestWord = "";
+    let currentWord = "";
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === " ") {
+            // End of a word
+            if (currentWord.length > longestWord.length) {
+                longestWord = currentWord;
+            }
+            currentWord = ""; // Reset current word
+            console.log("currentWord1", currentWord);
+        } else {
+            // Add character to current word
+            // console.log("str[i]", str[i])
+            currentWord += str[i];
+            // console.log("currentWord2", currentWord)
+        }
     }
-    words = str.split(" ")
-    return words.reduce((accuWord, currWord) => (currWord.length > accuWord.length ? currWord : accuWord), ""
-    );
+
+    // Check the last word in the string
+    if (currentWord.length > longestWord.length) {
+        longestWord = currentWord;
+    }
+
+    return longestWord;
 }
 
-console.log(findLongestWord("watch Alam Technical JavaScriptkjgjkv course on Youtube"))
+console.log(findLongestWord("watch Alam Technical JavaScriptkjgjkv course on Youtube"));
