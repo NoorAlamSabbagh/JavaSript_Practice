1dc7e99537d81456414cce32ffc6e4db79172075
NoorAlamSabbagh <nooralam7134310@gmail.com>
2025-02-27 05:23:11 +0530

    revise bitmanipulation

--- DSA_JS/DSA_Solved_In_JS/July/Lec13July11.js
+++ DSA_JS/DSA_Solved_In_JS/July/Lec13July11.js
@@ -34,10 +34,54 @@
 // leftShiftValue(a, b)
 
 //
-const RightShiftValue = (a, b) => {
-    console.log(a>>b);
+// const RightShiftValue = (a, b) => {
+//     console.log(a>>b);
+//     }
+//     let a = 11;
+//     // let b = 1;
+//     let b = 2;
+//     RightShiftValue(a, b)
+
+//
+//Masking in JavaScript
+// const masking = (a) => {
+//     if ((a & 1) === 0) {
+//         console.log("Even");
+//     } else {
+//         console.log("Odd");
+//     }
+// }
+// // let a = 5
+// let a = 10
+// masking(a)
+
+//
+//Way to find out bit is 1 or 0 in specific position of binary number
+// const bitPositon = (x, n) => {
+// if((x&(1<<(n-1)))===0){
+//     console.log("Zero")
+// }else{
+//     console.log("One")
+// }
+// }
+// let x = 77, n = 4;
+// bitPositon(x, n)
+
+//
+//To set bit from 0 to 1 or 1 to 0
+// const bitPositonSet = (x, n) => {
+//     let ans = (x|(1<<(n-1)))
+//         console.log("ans", ans)
+//     }
+//     // let x = 19, n = 2;
+//     let x = 19, n = 3;
+//     bitPositonSet(x, n)
+
+//
+const bitPositonUnSet = (x, n) => {
+    let ans = (x & (~(1<<(n-1))));
+        console.log("ans", ans)
     }
-    let a = 11;
-    // let b = 1;
-    let b = 2;
-    RightShiftValue(a, b)
+    // let x = 19, n = 2;
+    let x = 45, n = 4;
+    bitPositonUnSet(x, n)
