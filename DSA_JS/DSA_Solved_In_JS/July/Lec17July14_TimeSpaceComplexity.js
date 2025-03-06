ed2964c85142bfe1c40c3c49f76e04d5671ea04a
NoorAlamSabbagh <alamrangrej787@gmail.com>
2025-03-07 03:29:07 +0530

    Time Complexity

---
+++ DSA_JS/DSA_Solved_In_JS/July/Lec17July14_TimeSpaceComplexity.js
@@ -0,0 +1,34 @@
+// Function declaration can be hoisted
+// function timecomplexity() {
+//     let n = 20;
+//     // for (let i = 0; i <= n; i+2){//n/2
+//     // for (let i = 0; i <= n*2; i++){//2n
+//     for (let i = 0; i<Math.pow(2, n); i++){//2^n
+//         console.log(i+1);
+//     }
+// }
+// timecomplexity()
+
+//
+// function timecomplexity() {
+//     let n = 16;
+//     // for (let i = n; i>0; i--){//n
+//     // for (let i = n; i>0; i-= 2){//n/2
+//     // for (let i = n; i<Math.pow(2, n); i++){//Increasing exponentially of 2^n
+//     // for (let i = n; i>0; i=Math.floor(i/2)){//Decreasing exponentially of logn of base2 [log₂(n)]
+//     for (let i = n; i>0; i=Math.floor(i/3)){//log n base 3
+//         console.log(i);
+//     }
+// }
+// timecomplexity();
+
+//
+//some thing is increasing exponentially then log₂(n)
+//  2^3= 1,2,4,8 n = 3 i.e 
+
+//
+//some thing is decreasing exponentially then
+//  2^3= 1,2,4,8 n = 3 i.e 
+
+//
+//Asymptotic Notations
