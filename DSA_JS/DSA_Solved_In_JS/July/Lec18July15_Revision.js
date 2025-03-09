5b7be87d4e80993a57bb0e7d5cb72b85d431859b
NoorAlamSabbagh <alamrangrej787@gmail.com>
2025-03-10 03:47:02 +0530

    Recurssion

--- DSA_JS/DSA_Solved_In_JS/July/Lec18July15_Revision.js
+++ DSA_JS/DSA_Solved_In_JS/July/Lec18July15_Revision.js
@@ -1,30 +1,34 @@
-//Flames Problem
+// //Flames Problem
 
-function flamesProblem(arr1, arr2){
-    n = arr1.length;
-    m = arr2.length;
+// function flamesProblem(arr1, arr2) {
+//     n = arr1.length;
+//     m = arr2.length;
 
-    const flames = ["Siblings", "Friends", "Love", "Affection", "Marriage", "Enemy"];
+//     const flames = ["Siblings", "Friends", "Love", "Affection", "Marriage", "Enemy"];
 
-    let hashCount = 0
-    for(let i=0; i<n; i++){
-        for(let j = 0; j<m; j++){
-            if(arr1[i]=== arr2[j]){
-                arr1[i] = '#';
-                arr2[j] = '#';
-                hashCount += 2;
-                break;
-            }
+//     let hashCount = 0
+//     for (let i = 0; i < n; i++) {
+//         for (let j = 0; j < m; j++) {
+//             if (arr1[i] === arr2[j]) {
+//                 arr1[i] = '#';
+//                 arr2[j] = '#';
+//                 hashCount += 2;
+//                 break;
+//             }
 
-        }
-    }
-    // console.log('hashCopunt', hashCount);
-    let ans = (n + m - hashCount)%6
-console.log(flames[ans]);
+//         }
+//     }
+//     // console.log('hashCopunt', hashCount);
+//     let ans = (n + m - hashCount) % 6
+//     console.log(flames[ans]);
 
-}
+// }
 
-const str1 = "saumya".split(''); // Corrected method to convert to array
-const str2 = "ansh".split('');
+// const str1 = "saumya".split(''); // Corrected method to convert to array
+// const str2 = "ansh".split('');
 
-flamesProblem(str1, str2);
+// flamesProblem(str1, str2);
+
+
+//
+
--- DSA_JS/DSA_Solved_In_JS/July/Lec19July18_Recurssion.js
+++ DSA_JS/DSA_Solved_In_JS/July/Lec19July18_Recurssion.js
@@ -1,6 +1,82 @@
-function fun() {
-    for (let i = 1; i < 10; i++) {
-        console.log(`Function printed: Function${i}`);
-    }
-}
-fun()
+//<================Recursion==================>
+// Calling of a method inside its own definition
+//It should have a breaking condition else it will run into infinite loop.
+//Any task that can be done by iteration(looped) can be done using recursion and vice versa
+
+// function fun() {
+//     for (let i = 1; i < 10; i++) {
+//         console.log(`Function printed: Function${i}`);
+//     }
+// }
+// fun()
+
+
+//
+// function fun(i) {
+//     if (i === 10) return;
+
+//     console.log(i + " FUN!");
+//     fun(i + 1);
+// }
+
+// fun(0);
+
+//
+// function fun(k) {
+//     if(k === 3)
+//         return;
+//     for (let i = 1; i < 10; i++) {
+//         console.log(`Function printed: Function${i} -- ${k}`);
+//     }
+//     fun(k+1)
+// }
+// fun(1);
+
+//
+// function fun(k) {
+//     if (k === 3)
+//         return;
+//     for (let i = 1; i < 10; i++) {
+//         fun(k + 1)
+//         console.log(`Function printed: Function${i} -- ${k}`);
+//     }
+// }
+// fun(1);
+
+//
+//Any task that can be done by iteration(looped) can be done using recursion and vice versa
+//
+// function printDigits(n) {
+//     while (n > 0) {
+//         let r = n % 10;
+//         console.log("r", r);
+//         n = Math.floor(n / 10);
+//         console.log("n", n)
+//     }
+// }
+
+// // Example usage
+// let n = 1024;
+// printDigits(n);
+
+//
+//Same above tsks using recurssion
+// function printDigitsWithRecursion(n) {
+//     if (n === 0) return;
+    
+//     let r = n % 10;
+//     printDigitsWithRecursion(Math.floor(n / 10));
+//     process.stdout.write(r.toString()); // Print without newline
+// }
+
+// function printDigits(n) {
+//     while (n > 0) {
+//         let r = n % 10;
+//         console.log(r);
+//         n = Math.floor(n / 10);
+//     }
+// }
+
+// // Example usage
+// let n = 1024;
+// printDigitsWithRecursion(n);
