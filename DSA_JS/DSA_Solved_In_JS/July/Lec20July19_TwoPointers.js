44862287b0fe936cd5ecc25ca3285cd46fcca155
NoorAlamSabbagh <alamrangrej787@gmail.com>
2025-04-20 01:24:11 +0530

    two pointer

--- DSA_JS/DSA_Solved_In_JS/July/Lec20July19_TwoPointers.js
+++ DSA_JS/DSA_Solved_In_JS/July/Lec20July19_TwoPointers.js
@@ -1,82 +1,85 @@
-function twoArray(arr1, arr2, n, m) {
-    let arr = new Array(n + m); // Create an array with the required size
-    let i = 0;
+// function twoArray(arr1, arr2, n, m) {
+//     let arr = new Array(n + m); // Create an array with the required size
+//     let i = 0;
+    
+//     for (i = 0; i < n; i++) {
+//         arr[i] = arr1[i]
+//     }
+//     for (let j = 0; j < m; j++, i++) {
+//         arr[i] = arr2[j]
+//     }
+//     console.log("arr", arr)
+//     arr.sort((a, b)=> a-b);//Time complexity of sort is O(nlogn)
+//     return arr;
+// }
+// //
 
-    for (i = 0; i < n; i++) {
-        arr[i] = arr1[i]
-    }
-    for (let j = 0; j < m; j++, i++) {
-        arr[i] = arr2[j]
-    }
-    arr.sort((a, b)=> a-b);
-    return arr;
-}
-//
+// const arr1 = [2, 4, 10, 20, 30];
+// const arr2 = [1, 2, 15, 27, 60, 100, 200];
+// let n = arr1.length;
+// let m = arr2.length;
+// console.log(twoArray(arr1, arr2, n, m));
 
-const arr1 = [2, 4, 10, 20, 30];
-const arr2 = [1, 2, 15, 27, 60, 100, 200];
-let n = arr1.length;
-let m = arr2.length;
-console.log(twoArray(arr1, arr2, n, m));
-
+// Time Complexity of above code is O(n+m) for merging and O((n+m)log(n+m)) for sorting.
+// // So overall time complexity is O((n+m)log(n+m))
 // //TimeComplexity of above code is O((n+m)log(n+m))
 
 //
 //<============= Now We are using Two Pointer method ===============>
-// function twoPointer(arr1, arr2, n, m) {
-//     let arr = [];
-//     let i = 0, j = 0, k = 0
+function twoPointer(arr1, arr2, n, m) {
+    let arr = [];
+    let i = 0, j = 0, k = 0 //k is used to track the position in the merged array
 
-//     while (i < n && j < m) {
-//         if (arr1[i] < arr2[j]) {
-//             arr[k] = arr1[i]
-//             // arr.push(arr1[i]);
-//             i++;
-//             // k++;
-//         } else if (arr1[i] > arr2[j]) {
-//             arr[k] = arr2[j];
-//             j++;
-//             // k++;
-//         } else {
-//             // Both are equal, so add both
-//             arr[k] = arr1[i];
-//             i++;
-//             k++;
-//             arr[k] = arr2[j];
-//             j++;
-//             k++;
-//         }
-//         k++;
-//     }
+    while (i < n && j < m) {
+        if (arr1[i] < arr2[j]) {
+            arr[k] = arr1[i]
+            // arr.push(arr1[i]);
+            i++;
+            // k++;
+        } else if (arr1[i] > arr2[j]) {
+            arr[k] = arr2[j];
+            j++;
+            // k++;
+        } else {
+            // Both are equal, so add both
+            arr[k] = arr1[i];
+            i++;
+            // k++;
+            arr[k] = arr2[j];
+            j++;
+            // k++;
+        }
+        k++;
+    }
 
-//     while (i < n) {
-//         arr[k] = arr1[i];
-//         i++;
-//         k++
-//     }
-//     while (j < n) {
-//         arr[k] = arr2[j];
-//         j++;
-//         k++
-//     }
-//     return arr;
-// }
+    while (i < n) {
+        arr[k] = arr1[i];
+        i++;
+        k++
+    }
+    while (j < n) {
+        arr[k] = arr2[j];
+        j++;
+        k++
+    }
+    return arr;
+}
 
-// const arr1 = [1, 2, 2, 6, 6, 10];// i
-// const arr2 = [2, 4, 10, 15, 20, 30];//j
-// //Step1: I will compare arr1, arr2 index which value is smaller I will put it in new arr 
-// // arr = [1]
-// // and I will increase index which arr1 or arr2 value is smaller, Now in the above i at arr[1] = 2 and j at arr[0] = 2 
-// //Now in this case I will move both index, 
-// //arr = [1, 2, 2, 2]
-// // now i of arr1 is at arr[3] = 6 and j of arr2 is at arr2[1] = 4, Now arr2 is smaller so I put arr2 vale and move j index
-// //arr[1, 2, 2, 2, 4] and it will continue
-// // arr[1, 2, 2, 2, 4, 6, 6, 10, 10, 15, 20, 30]
-// //Its time complexity is O(n+m)
+const arr1 = [1, 2, 2, 6, 6, 10];// i
+const arr2 = [2, 4, 10, 15, 20, 30];//j
+//Step1: I will compare arr1, arr2 index which value is smaller I will put it in new arr 
+// arr = [1]
+// and I will increase index which arr1 or arr2 value is smaller, Now in the above i at arr[1] = 2 and j at arr[0] = 2 
+//Now in this case I will move both index, 
+//arr = [1, 2, 2, 2]
+// now i of arr1 is at arr[3] = 6 and j of arr2 is at arr2[1] = 4, Now arr2 is smaller so I put arr2 vale and move j index
+//arr[1, 2, 2, 2, 4] and it will continue
+// arr[1, 2, 2, 2, 4, 6, 6, 10, 10, 15, 20, 30]
+//Its time complexity is O(n+m)
 
-// let n = arr1.length;
-// let m = arr2.length;
-// console.log(twoPointer(arr1, arr2, n, m)); 
+let n = arr1.length;
+let m = arr2.length;
+console.log(twoPointer(arr1, arr2, n, m)); 
 
 
 ////////////
